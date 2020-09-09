e648be9e14377cf6a2cc3e862996b4a714cfd151
Sean <sean.lu91@gmail.com>
2020-09-09 09:22:13 +0800

    Add NewBridge.

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,6 +1,10 @@
 package bridge
 
-import "math/big"
+import (
+	"github.com/oinfinance/crossnode/bridge/ethereum"
+	"github.com/oinfinance/crossnode/bridge/ontology"
+	"math/big"
+)
 
 type Address []byte
 type Receipt []byte
@@ -13,4 +17,13 @@ type Bridge interface {
 	GetBalance(account Address) *big.Int
 	Transfer(from, to Address, value *big.Int) Receipt
 	BatchTransfer(from Address, to []Address, value []*big.Int) Receipts
+}
+
+func NewBridge(chainId uint) Bridge {
+	switch chainId {
+	case 0x01:
+		return &ethereum.EthBridge{}
+	case 0x02:
+		return &ontology.OntBridge{}
+	}
 }
