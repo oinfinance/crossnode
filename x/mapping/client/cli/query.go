9c2aeca6a6bbabcb1613aaec3246f04b1132f51c
Sean <sean.lu91@gmail.com>
2020-09-11 02:20:01 +0800

    add tx and query cmd for mapping.

--- x/mapping/client/cli/query.go
+++ x/mapping/client/cli/query.go
@@ -1,14 +1,16 @@
 package cli
 
 import (
+	"fmt"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/oinfinance/crossnode/x/mapping/types"
 	"github.com/spf13/cobra"
 )
 
 // GetTxCmd returns the transaction commands for this module
-func GetQueryCmd(cdc *codec.Codec) *cobra.Command {
+func GetQueryCmd(route string, cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:                        types.ModuleName,
 		Short:                      "Querying commands for the auth module",
@@ -17,7 +19,34 @@ func GetQueryCmd(cdc *codec.Codec) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	//cmd.AddCommand(GetAccountCmd(cdc))
+	cmd.AddCommand(client.GetCommands(
+		GetCmdQueryMapinfo(route, cdc))...)
 
 	return cmd
 }
+
+func GetCmdQueryMapinfo(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use: "mapping [cross_address]",
+		Short: "Get corss address's map info",
+		Args:cobra.ExactArgs(1),
+		RunE:func(cmd *cobra.Command, args []string) error {
+			clictx:= context.NewCLIContext().WithCodec(cdc)
+			bz, err := cdc.MarshalJSON(types.QueryInfoByLocalParams{LocalAddr: args[0]})
+			if err != nil {
+				return err
+			}
+			route := fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryInfoByLocalParams{})
+			res, _, err := clictx.QueryWithData(route, bz)
+			if err != nil {
+				return fmt.Errorf("could not resolve: %s", err)
+			}
+			var resp types.QueryInfoResponse
+			if err =cdc.UnmarshalJSON(res, &resp); err != nil {
+				return err
+			}
+			return clictx.PrintOutput(resp)
+		},
+	}
+}
+
--- x/mapping/client/cli/tx.go
+++ x/mapping/client/cli/tx.go
@@ -1,8 +1,14 @@
 package cli
 
 import (
+	"errors"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/oinfinance/crossnode/bridge"
 	"github.com/oinfinance/crossnode/x/mapping/types"
 	"github.com/spf13/cobra"
 )
@@ -11,13 +17,45 @@ import (
 func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	txCmd := &cobra.Command{
 		Use:                        types.ModuleName,
-		Short:                      "Auth transaction subcommands",
+		Short:                      "mapping transaction subcommands",
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
-	//txCmd.AddCommand(
-	//	GetMultiSignCommand(cdc),
-	//)
+	txCmd.AddCommand(client.PostCommands(
+		GetCmdRegister(cdc),
+	)...)
 	return txCmd
+}
+
+func GetCmdRegister(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use: "register [token_address] [chainName] [tokenName] to [crossnode_address]",
+		Short:"register remote chain token to corssnode address",
+		Args: cobra.ExactArgs(4),
+		RunE: func (cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			oinAddress, err := sdk.AccAddressFromHex(args[0])
+			if err != nil {
+				return err
+			}
+			chainName := args[1]
+			tokenName := args[2]
+			if !bridge.SupportedByName(chainName, tokenName) {
+				return errors.New("unsupported chain token pair")
+			}
+
+			crossAddress, err := sdk.AccAddressFromHex(args[3])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgRegister(oinAddress.Bytes(), uint(bridge.ChainIdByName(chainName)), uint(bridge.TokenIdByName(tokenName)),
+				crossAddress.Bytes())
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
 }
